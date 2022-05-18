5281c3b0a5363d154bf082c029f59aab4f02d9d5
Parth Bisht <bishtparth795@gmail.com>
2022-05-18 20:26:29 +0530

    updated

--- day3/customhook/src/App.tsx
+++ day3/customhook/src/App.tsx
@@ -1,13 +1,24 @@
 
 import './App.css';
+import useFetch from './hooks/UseFetch';
 import useTimeout from './hooks/UseTimeout';
+
+const URL = `https://api.github.com/search/users?q=Parth-Bisht`;
+
 
 function App() {
   const ready = useTimeout(5000);
+  const {loading,data,error} = useFetch(URL)
 
   return (
     <div className="App">
       {ready?<div>I am ready</div>:<div>Waiting for timer</div>}
+      { loading? <div>Loading....</div> : error? <div>Something went wrong</div> : data.map((user)=>{
+          return <div key={user.id}>
+            <h3>{user.login}</h3>
+            <img style={{borderRadius:"50%"}} src={user.avatar_url} alt="" />
+          </div>
+        })}
     </div>
   );
 }
