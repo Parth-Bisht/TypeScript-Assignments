ac1def44142f19b4d62b3d95e7c49609d7a74809
Parth Bisht <bishtparth795@gmail.com>
2022-05-19 01:24:55 +0530

    country-search

--- day3/country-search/src/App.tsx
+++ day3/country-search/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import InputBox from './components/InputBox';
+import { useDebouncedCallback } from '@react-hookz/web';
 
 function App() {
+  const [value,setValue] = useState<string>("")
+
+  
+  const onChangeDebounced = useDebouncedCallback(
+    (e)=>setValue(e.target.value),
+    [],
+    1000,
+    0
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputBox label='value' value={value} onChange={onChangeDebounced} />
     </div>
   );
 }
