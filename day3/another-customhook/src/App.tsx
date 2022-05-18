1cd29ecf4d602cad63e6ec4446085a0583151069
Parth Bisht <bishtparth795@gmail.com>
2022-05-18 23:25:02 +0530

    commit

---
+++ day3/another-customhook/src/App.tsx
@@ -0,0 +1,30 @@
+import {SyntheticEvent} from 'react';
+import './App.css';
+import useMergeState from './hooks/UseMerge';
+
+// type formType = {
+//   username: string;
+//   email: string;
+//   password: string;
+// }
+
+function App() {
+
+const state  = useMergeState() 
+  const handleSubmit = (e:SyntheticEvent)=>{
+    e.preventDefault()
+    console.log(state)
+  }
+  return (
+    <div className="App">
+     <form onSubmit={handleSubmit} >
+       <input name='username' type="text" placeholder='Enter Username' />
+       <input name='email' type="email" placeholder='Enter Email' />
+       <input name='password' type="password" placeholder='Enter Password' />
+       <input type="submit" value="submit" />
+     </form>
+    </div>
+  );
+}
+
+export default App;
